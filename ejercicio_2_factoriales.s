ac3e013449fa772abc2d24345c7d66c695c823a1
GABOBB <gabbol2002@gmail.com>
2025-05-19 00:02:26 -0600

    solucion en problema 2

--- ejercicio_2_factoriales.s
+++ ejercicio_2_factoriales.s
@@ -6,29 +6,30 @@ _start:
     bl      factorial       
     mov     r4, r1          
 
-    // PRUEBA 2: X = 5
+    // PRUEBAX = 5
     mov     r0, #5
     bl      factorial
-    mov     r5, r1          @ Guardar resultado en r5
+    mov     r5, r1          
 
-    // PRUEBA 3: X = 6
+    // PRUEBApX = 6
     mov     r0, #6
     bl      factorial
-    mov     r6, r1          @ Guardar resultado en r6
+    mov     r6, r1          
 
-    // Fin del programa (puedes poner un bucle infinito si lo corres en bare metal)
-    b       .
+    
+           
 
 factorial:
-    mov     r1, #1          @ r1 = resultado
+    mov     r1, #1          
     cmp     r0, #0
     beq     end_factorial
 
 factorial_loop:
-    mul     r1, r1, r0      @ r1 = r1 * r0
-    sub     r0, r0, #1      @ r0--
+    mul     r1, r1, r0      
+    sub     r0, r0, #1      
     cmp     r0, #0
     bgt     factorial_loop
 
 end_factorial:
-    bx      lr              @ return
+    bx      lr              
+
